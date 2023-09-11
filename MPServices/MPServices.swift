ecd46a6d200caf1a2ffccb19fcf47448f91cc8d8
Manole Daniela <danimanole99@gmail.com>
2023-09-11 19:40:22 +0300

    Build no. update + Compute Score Service

--- MPServices/MPServices.swift
+++ MPServices/MPServices.swift
@@ -5,6 +5,16 @@
 //  Created by Daniela Manole on 11.09.2023.
 //
 
-class MPServices {
-
+public class MPServices {
+    public class func computeScore(numberOfSeconds: Int, numberOfTries: Int, numberOfMatches: Int, numberOfCards: Int) -> Int {
+        var score = 0
+        
+        let timeScore = ((45 - numberOfSeconds) / 45) * 20 // 20%
+        let triesScore = 30 / (numberOfTries - numberOfCards + 1) // 30%
+        let matchScore = (numberOfMatches / numberOfCards) * 50 // 50%
+        
+        score = timeScore + triesScore + matchScore
+        
+        return score
+    }
 }
